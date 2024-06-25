c36ea1340f572bcedb08fd379a70c1a6f0da3024
Tariq-act <khantarikact@gmail.com>
2024-06-25 15:16:24 +0530

    next-auth sign with google

--- app/loggedin/page.tsx
+++ app/loggedin/page.tsx
@@ -1,14 +1,13 @@
 import { signInAction } from '@/actions/auth-action';
+import Tasks from '@/components/todovex/tasks';
 import { Button } from '@/components/ui/button';
 
 export default function Home() {
   return (
     <main className='flex flex-col items-center justify-between p-24'>
-      <h1>Login</h1>
+      <h1>Todovex</h1>
 
-      <form action={signInAction}>
-        <Button>Log in</Button>
-      </form>
+      <Tasks />
     </main>
   );
 }
