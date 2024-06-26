13546ebe2687c51310442ec253cd469f732f83a9
Tariq-act <khantarikact@gmail.com>
2024-06-26 16:59:53 +0530

    add authentication betweeen authjs and convex

--- app/loggedin/page.tsx
+++ app/loggedin/page.tsx
@@ -1,13 +1,19 @@
 import { signInAction } from '@/actions/auth-action';
+import SideBar from '@/components/nav/side-bar';
 import Tasks from '@/components/todovex/tasks';
-import { Button } from '@/components/ui/button';
+import UserProfile from '@/components/todovex/user-profile';
 
 export default function Home() {
   return (
-    <main className='flex flex-col items-center justify-between p-24'>
-      <h1>Todovex</h1>
-
-      <Tasks />
-    </main>
+    <div className='grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]'>
+      <SideBar />
+      <div className='flex flex-col'>
+        <main className='flex flex-1 flex-col gap-4 p-4 lg:px-8'>
+          <h1>Todovex</h1>
+          <UserProfile />
+          <Tasks />
+        </main>
+      </div>
+    </div>
   );
 }
